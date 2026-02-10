9fc2d96012fdfb4cdebc98df3364276b0abe076a
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-02-10 22:58:05 +0100

    added parser for datetime

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import streamlit as st
+from dateutil import parser
 
 # Load Data
 DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
@@ -11,7 +12,7 @@ def load_data():
     if not os.path.exists(catalog_path):
         return None
     df = pd.read_csv(catalog_path)
-    df['time'] = pd.to_datetime(df['time'])
+    df['time'] = df['time'].apply(parser.parse)
     return df
 
 df = load_data()
